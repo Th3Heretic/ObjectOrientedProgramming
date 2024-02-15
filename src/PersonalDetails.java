f18889537ded4039e3937492c47e5495fc63b798
Th3Heretic <146324718+Th3Heretic@users.noreply.github.com>
2024-02-15 14:12:54 +0000

    Week one uploaded

---
+++ src/PersonalDetails.java
@@ -0,0 +1,10 @@
+public class PersonalDetails
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Damien Snow");
+        System.out.println("117a Church Road");
+        System.out.println("24 Years old");
+        System.out.println("07515947525");
+    }
+}
